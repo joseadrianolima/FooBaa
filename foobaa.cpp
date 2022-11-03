89630144e752fbda131481d1ac37f156bfe390fd
José Adriano de Lima <adriano.lima.dk@gmail.com>
2022-11-03 19:49:01 -0300

    First commit

---
+++ foobaa.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        int modType = 3 * ((i % 3) == 0) + 5 * ((i % 5) == 0);
+        switch (modType)
+        {
+        case 3:
+            cout << "Foo" << endl;
+            break;
+        case 5:
+            cout << "Baa" << endl;
+            break;
+        case 8:
+            cout << "FooBaa" << endl;
+            break;
+        default:
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
